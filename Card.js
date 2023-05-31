e489f007f0cedf92f6256db69d1c5d3887710242
ajm2017 <32173503+ajm2017@users.noreply.github.com>
2023-05-30 23:53:16 -0400

    starting cards library

--- Card.js
+++ Card.js
@@ -1,13 +1,13 @@
 // Function to create a standard 52-card deck
 function createDeck(v) {
-    var suits = ['♠', '♣', '♥', '♦'];
+    var suits = ['s', 'c', 'h', 'd'];
     var ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
-    d = [];
+    var d = [];
     for (var i = 0; i < suits.length; i++) {
       for (var j = 0; j < ranks.length; j++) {
         d.push(ranks[j] + suits[i]);
       }
     }
-    if (v) console.log("Deck created:", deck);
+    if (v) console.log("Deck created:", d);
     return d;
 }
