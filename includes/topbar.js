1de7ee7bfc90822873e5acdc53e92e85c3c694a5
ajm2017 <32173503+ajm2017@users.noreply.github.com>
2024-01-31 20:18:45 -0500

    top bar

--- includes/topbar.js
+++ includes/topbar.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     const topBar = document.createElement('div');
     topBar.style.cssText = 'position: fixed; top: 0; left: 0; width: 100%; background: grey; color: white; padding: 2px; text-align: left; z-index: 1000;';
-    topBar.innerHTML = `<a href="../" style="color: white; text-decoration: none;"><b>Card.js</b></a> | ${titleSuffix}`;
+    topBar.innerHTML = `<a href="../" style="color: white; text-decoration: none;"><b>Card.js</b></a> | <i>${titleSuffix}</i>`;
     document.body.prepend(topBar);
 });
